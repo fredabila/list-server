b17c68eb088333fb3e8d319a375501bc9b3d0c27
frederickabila <sharksinternet@gmail.com>
2024-08-15 21:22:52 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let selectedQrCodes = new Set(); // Use a set to keep track of selected QR codes
+
+app.get('/available-qr-codes', (req, res) => {
+    const availableQrCodes = Array.from({ length: 500 }, (_, i) => i + 1).filter(num => !selectedQrCodes.has(num));
+    res.json(availableQrCodes);
+});
+
+app.post('/select-qr-code', (req, res) => {
+    const { number } = req.body;
+    if (number && !selectedQrCodes.has(number)) {
+        selectedQrCodes.add(number);
+        res.json({ message: 'QR code selected successfully.' });
+    } else {
+        res.status(400).json({ message: 'QR code already selected or invalid.' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
